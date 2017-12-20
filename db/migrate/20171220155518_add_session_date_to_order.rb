45135d63135e6e1fb576dde3af5f058c62467b62
Sergei Dzemidovich <n3mfis@gmail.com>
2017-12-20 17:23:02 +0100

    fix

--- db/migrate/20171220155518_add_session_date_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddSessionDateToOrder < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :orders, :session_date, foreign_key: true
-  end
-end
--- db/migrate/20171220161030_add_photo_count_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoCountToOrder < ActiveRecord::Migration[5.1]
-  def change
-    add_column :orders, :photo_count, :integer
-  end
-end
