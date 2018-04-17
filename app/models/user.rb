6e3395519f0721b8d6a3dd88db77722b59547946
Dumitru <raileandumitru@protonmail.ch>
2018-04-17 14:18:45 +0300

    Add user role and profile, delegate user info to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :profile
   has_many :locations
   has_many :invoices
   has_many :photo_sessions
@@ -11,6 +12,14 @@ class User < ApplicationRecord
   has_and_belongs_to_many :langs
 
   has_many :orders
+  accepts_nested_attributes_for :profile
+
+  after_create :create_profile
+
+  enum role: {
+    client: 0,
+    photographer: 1
+  }
 
   has_attached_file :avatar, styles: { medium: "64x64#", thumb: "16x16#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
