bf4c43d12c00ee3c1be3cb5d23ffdcd2a5691ed7
Dumitru <raileandumitru@protonmail.ch>
2018-04-17 14:26:06 +0300

    Refactor header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def active_path(path)
+    if current_page?(path)
+      'nav-item active'
+    else
+      'nav-item'
+    end
+  end
+
+  def profile_defined_path
+    if current_user.client?
+      client_path
+    elsif current_user.photographer?
+      photographer_path
+    end
+  end
 end
