e91dd9a9d07f9222d27956f0173e713f1824096d
Dumitru <raileandumitru@protonmail.ch>
2018-04-18 16:45:57 +0300

    Change git project repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.1'
 
 set :application, 'bookaphotos.com'
-set :repo_url, 'git@github.com:developerby/bookaphotos.com.git'
+set :repo_url, 'git@gitlab.com:PhotoBookingSystem/bookaphotos.com.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
