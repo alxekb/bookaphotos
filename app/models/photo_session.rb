e193cb1b72e08cedb058c88e71ddd42a195f6107
Dumitru <raileandumitru@protonmail.ch>
2018-04-23 18:34:27 +0300

    Photosession validation and tests

--- app/models/photo_session.rb
+++ app/models/photo_session.rb
@@ -26,6 +26,29 @@ class PhotoSession < ApplicationRecord
   has_many :session_days, :dependent => :destroy
   accepts_nested_attributes_for :session_days, allow_destroy: true, reject_if: ->(a) { a[:id].nil? && a[:price].nil? }
 
+  validates :title,
+           :description,
+           :price,
+           :user_id,
+           :currency_id,
+           :duration,
+           :photos_count,
+           :period_of_execution,
+           :price_per_photo,
+           :for_whom,
+           :preparation,
+           :what_to_take,
+           :how_route,
+           :how_find,
+           :lat,
+           :lng, presence: true
+  validates :price,
+            :price_per_photo,
+            :duration,
+            :period_of_execution,
+            :photos_count, numericality: { greater_than_or_equal_to: 1 }
+  validates :published, inclusion: { in: [true, false] }
+
   def self.touch
     update_all(updated_at: Time.now)
   end
---
+++ spec/factories/photo_sessions.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :photo_session do
+    title { Faker::GameOfThrones.city }
+    description { Faker::GameOfThrones.quote }
+    price { Faker::Number.between(1, 5000) }
+    published { Faker::Boolean.boolean(0.5) }
+    duration { Faker::Number.between(10, 120) }
+    photos_count { Faker::Number.between(10, 600) }
+    period_of_execution { Faker::Number.between(10.30) }
+    price_per_photo { Faker::Number.between(1, 100) }
+    for_whom { Faker::GameOfThrones.character }
+    preparation { Faker::GameOfThrones.house }
+    what_to_take { Faker::GameOfThrones.dragon }
+    how_route { Faker::RickAndMorty.location }
+    how_find { Faker::RickAndMorty.quote }
+    lat { "#{Faker::Number.decimal(2, 10) }" }
+    lng { "#{Faker::Number.decimal(2, 10) }" }
+    user
+    currency
+  end
+end
