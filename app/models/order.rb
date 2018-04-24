b6de2056c22f0d355cf109d64342556d3caa72fb
Dumitru <raileandumitru@protonmail.ch>
2018-04-24 20:01:34 +0300

    Add validation and tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,35 @@
 class Order < ApplicationRecord
+  include EmailValidateable
   belongs_to :user
   belongs_to :photo_session
   belongs_to :session_day
+
+  validates :name,
+            :phone,
+            :user_id,
+            :photo_session_id,
+            :session_day_id,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false }, email: true
+  validates :comment, presence: true, allow_nil: true, allow_blank: true
+  validates :photo_count,
+            numericality: { greater_than_or_equal_to: 1 },
+            allow_blank: true,
+            allow_nil: true
+  validates :i_accept_term,
+            :i_consent_personal_data,
+            inclusion: { in: [true] }
+  validates :i_want_to_get_info, inclusion: { in: [true, false] }
+  validate :ensure_user_agreement
+
+  private
+
+  def ensure_user_agreement
+    if !self.i_consent_personal_data && !self.i_accept_term
+      errors.add(:i_consent_personal_data, "You must agree to continue.")
+      errors.add(:i_accept_term, "You must accept term's to continue")
+    end
+  end
 end
