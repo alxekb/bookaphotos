4a9232b1ce86e4624a125a8aad1bf9f2ae62de5c
Dumitru <raileandumitru@protonmail.ch>
2018-04-24 18:58:23 +0300

    Add session day validation and test

---
+++ spec/models/session_day_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe SessionDay, type: :model do
+  it "has a valid factory" do
+    expect(build(:session_day)).to be_valid
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:start_time) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:photo_session_id) }
+    it { is_expected.to validate_presence_of(:currency_id) }
+
+    it { is_expected.to belong_to(:photo_session) }
+    it { is_expected.to belong_to(:currency) }
+  end
+end
