dd50789b30dd4fabc76982dda369b02866f647e3
Dumitru <raileandumitru@protonmail.ch>
2018-04-25 11:29:56 +0300

    Validate title uniqueness

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,7 +1,9 @@
 class Currency < ApplicationRecord
   has_many :photo_sessions
 
-  validates :title, presence: true
+  validates :title,
+            presence: true,
+            uniqueness: { case_sensitive: false }
 
   def to_s
     title
