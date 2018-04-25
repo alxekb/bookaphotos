75e59a7e1efcf364a041efe8f0c415b8e4a191c8
Dumitru <raileandumitru@protonmail.ch>
2018-04-25 12:13:13 +0300

    Add valdiation and test for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,12 @@
 class Location < ApplicationRecord
   belongs_to :user
+
+  validates :name,
+            :address,
+            :how_to_find,
+            :lat,
+            :lng,
+            :user_id, presence: true
+  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
---
+++ spec/models/location_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Location, type: :model do
+  it "has a valid factory" do
+    expect(build(:location)).to be_valid
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:lat) }
+    it { is_expected.to validate_presence_of(:lng) }
+    it { is_expected.to validate_presence_of(:user_id) }
+    it { is_expected.to validate_numericality_of(:lat).is_greater_than_or_equal_to(-90) }
+    it { is_expected.to validate_numericality_of(:lng).is_greater_than_or_equal_to(-180) }
+    it { is_expected.to validate_numericality_of(:lat).is_less_than_or_equal_to(90) }
+    it { is_expected.to validate_numericality_of(:lng).is_less_than_or_equal_to(180) }
+  end
+end
