a064d1fb27452d77f5d5769c4dfdb18c9dd87460
Dumitru <raileandumitru@protonmail.ch>
2018-05-01 18:06:08 +0300

    Set permission

--- app/controllers/profile/photographer_controller.rb
+++ app/controllers/profile/photographer_controller.rb
@@ -1,2 +1,9 @@
 class Profile::PhotographerController < ProfileController
+  before_action :require_permission
+
+  private
+
+  def require_permission
+    redirect_to root_path unless current_user.photographer?
+  end
 end
