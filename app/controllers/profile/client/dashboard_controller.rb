d7d7c9c01720c3948e7461a8fcd45095c6e4b360
Dumitru <raileandumitru@protonmail.ch>
2018-05-01 18:06:58 +0300

    Decorate client for dashboard

--- app/controllers/profile/client/dashboard_controller.rb
+++ app/controllers/profile/client/dashboard_controller.rb
@@ -1,15 +1,5 @@
 class Profile::Client::DashboardController < ProfileController
   def index
-    @user = current_user
-    @orders = current_user.orders
-  end
-
-  def create
-    user = current_user
-    user.phone = params[:user][:phone]
-    user.name = params[:user][:name]
-    user.save
-
-    redirect_to client_path, notice: "Setting was updated"
+    @user = current_user.decorate
   end
 end
