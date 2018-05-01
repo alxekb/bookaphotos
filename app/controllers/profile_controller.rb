47d57d2af85047b19ee9d3132fe5bbc3c16ff9df
Dumitru <raileandumitru@protonmail.ch>
2018-05-01 18:03:41 +0300

    Decorate user, set correct layout

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,20 +1,27 @@
 class ProfileController < ApplicationController
   before_action :authenticate_user!
   before_action :profile
+  before_action :set_user
 
   layout :set_layout
 
+  protected
+
   def profile
-    current_user.profile
+    @user_profile = current_user.profile
   end
 
   def set_layout
     if current_user.client?
-      "application"
+      "client"
     elsif current_user.photographer?
       "photographer"
     else
       "application"
     end
+  end
+
+  def set_user
+    @user = current_user.decorate
   end
 end
