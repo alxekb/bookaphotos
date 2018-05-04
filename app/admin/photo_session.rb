c6d3703d03adf39cb4f57424e0268ad09b1810ef
Dumitru <raileandumitru@protonmail.ch>
2018-05-04 21:17:27 +0300

    Refactor admin photo session form

--- app/admin/photo_session.rb
+++ app/admin/photo_session.rb
@@ -35,6 +35,8 @@ ActiveAdmin.register PhotoSession do
         s.input :start_time, as: :just_datetime_picker
         s.input :special
         s.input :price
+        s.input :price_per_additional_photo
+        s.input :additional_photos_limit
         s.input :currency
         s.input :_destroy, as: :boolean, required: false, label: "Remove" if s.object.present?
       end
@@ -44,23 +46,21 @@ ActiveAdmin.register PhotoSession do
       f.input :duration
       f.input :photos_count
       f.input :period_of_execution
-      f.input :price
       f.input :currency
-      f.input :price_per_photo
       f.input :cities, as: :check_boxes
       f.input :themes, as: :check_boxes
 
-      f.inputs do
-        f.input :lat
-        f.input :lng
-        f.latlng lang: :en, api_key: "AIzaSyAvgY2gVt4mWuyAfJbBfLTMnl_Vbsd_Ydw", map: :google
-      end
+      # f.inputs do
+      #   f.input :lat
+      #   f.input :lng
+      #   f.latlng lang: :en, api_key: "AIzaSyAvgY2gVt4mWuyAfJbBfLTMnl_Vbsd_Ydw", map: :google
+      # end
 
       f.input :for_whom
       f.input :preparation
       f.input :what_to_take
-      f.input :how_route
-      f.input :how_find
+      # f.input :how_route
+      # f.input :how_find
     end
     f.actions
   end
