27d2835c4e96481849f3310d5e177d54d9f15d2c
Dumitru <raileandumitru@protonmail.ch>
2018-05-04 20:40:50 +0300

    Add session day new validation test

--- spec/factories/session_days.rb
+++ spec/factories/session_days.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     start_time { Faker::Time.forward(2, :morning) }
     special { Faker::Boolean.boolean(0.5) }
     price { Faker::Number.between(1, 5000) }
+    price_per_additional_photo { Faker::Number.between(1, 5000) }
+    additional_photos_limit { Faker::Number.between(1, 5000) }
   end
 end
