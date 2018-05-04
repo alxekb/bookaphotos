bf553c51b99a961a1eb54cffd97fcbe444cdcaba
Dumitru <raileandumitru@protonmail.ch>
2018-05-04 20:41:08 +0300

    Fix letter error

--- spec/models/session_day_spec.rb
+++ spec/models/session_day_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe SessionDay, type: :model do
     it { is_expected.to validate_presence_of(:photo_session_id) }
     it { is_expected.to validate_presence_of(:currency_id) }
     it { is_expected.to validate_presence_of(:price_per_additional_photo) }
-    it { is_expected.to validate_presence_of(:additional_photos_limi) }
+    it { is_expected.to validate_presence_of(:additional_photos_limit) }
 
     it { is_expected.to belong_to(:photo_session) }
     it { is_expected.to belong_to(:currency) }
