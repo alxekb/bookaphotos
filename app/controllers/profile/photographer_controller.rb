b4254bd97e55e2596db99e5ba2a57faea1dcca20
Dumitru <raileandumitru@protonmail.ch>
2018-05-10 18:56:32 +0300

    Set decorated photographer for sidebar partial

--- app/controllers/profile/photographer_controller.rb
+++ app/controllers/profile/photographer_controller.rb
@@ -1,7 +1,12 @@
 class Profile::PhotographerController < ProfileController
   before_action :require_permission
+  before_action :set_photographer
 
   private
+
+  def set_photographer
+    @photographer = current_user.decorate
+  end
 
   def require_permission
     redirect_to root_path unless current_user.photographer?
