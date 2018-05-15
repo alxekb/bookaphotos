10689e3d639fdb288f9acc58a566c3d728dfb6c8
Dumitru <raileandumitru@protonmail.ch>
2018-05-15 14:21:22 +0300

    Set manually the keys for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,6 @@
+# currently using only test environment
 Rails.configuration.stripe = {
-    publishable_key: Rails.application.secrets.stripe_publishable_key,
-    secret_key: Rails.application.secrets.stripe_secret_key
+    publishable_key: "pk_test_x7w09S73tXOnQ0RdlmOx7oj9",
+    secret_key: "sk_test_4BvYP85iWPsd7SsvvOtSRBZu"
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
